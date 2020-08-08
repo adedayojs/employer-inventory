662fa8b27f0d6c2fa054916248748d72adafd361
adedayojs <samfeolu@gmail.com>
2020-08-08 20:25:42 +0100

    (Build): Add files to facilitate build oin server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var serveStatic = require("serve-static");
+const app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+// eslint-disable-next-line no-console
+console.log("server started " + port);
+
+app.use(function(req, res) {
+  res.status(404).json({ err: "error" });
+});
